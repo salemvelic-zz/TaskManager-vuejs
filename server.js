e1c2acdfc6f7c0d7317f80541a0b97f98947606b
svelic <velicsalem@gmail.com>
2018-04-13 22:45:37 +0200

    final touch

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-'use strict';
-
-const path = require('path');
-const app = require('express')();
-
-const port = process.env.PORT || 8080;
-
-app.use('/', express.static(path.join(__dirname, 'testheroku')));
-
-app.listen(process.env.PORT || 8080);
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+app = express();
+app.use(serveStatic(__dirname + "/dist"));
+var port = process.env.PORT || 8080;
+app.listen(port);
+console.log('server started '+ port);
